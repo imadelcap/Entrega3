c960fb157dccf18b06443f9c2d62c0594211b992
Imanol Calvo <calvolan@gmail.com>
2023-12-03 18:39:56 -0300

    Problemas solucionados. Aplicacion y pagina funcionando. Solo resta por pulir detalles esteticos.

--- AppTiendaBicicletas/views.py
+++ AppTiendaBicicletas/views.py
@@ -7,17 +7,17 @@ def inicio(request):
 
 def formulario_modelo_bicicleta(request):
     if request.method == 'POST':
-        modelo_bicicleta = ModeloBicicleta(request.POST['marca'], request.POST['tipo'], request.POST['rodado'])
+        #modelo_bicicleta = ModeloBicicleta(request.POST['marca'], request.POST['tipo'], request.POST['rodado'])
+        modelo_bicicleta = ModeloBicicleta(marca = request.POST['marca'], tipo = request.POST['tipo'], rodado = request.POST['rodado'])
         modelo_bicicleta.save()
-        return render(request, 'inicio.html')    
+        return render(request, 'index.html')    
     
     return render(request, 'formulario_modelo_bicicleta.html')
 
-def formulario_modelo_bicicleta1(request): # con este metodo no me generaba el objeto formulario, solo me aparecia el boto de submit. Por eso le cambie el nombre a la funcion
+def formulario_modelo_bicicleta1(request): # con este metodo no me generaba el objeto formulario, solo me aparecia el boto de submit. Por eso le cambie el nombre a la funcion. Luego corregi el segundo return y aplique los cambios a los otros formularios
     if request.method == 'POST':
         formulario = ModeloBicicletaFormulario(request.POST)
-        print(formulario)
-
+        
         if formulario.is_valid():
             datos = formulario.cleaned_data
             modelo_bicicleta = ModeloBicicleta(datos['marca'], datos['tipo'], datos['rodado'])
@@ -26,18 +26,19 @@ def formulario_modelo_bicicleta1(request): # con este metodo no me generaba el o
     
     
     formulario = ModeloBicicletaFormulario(request.POST)
+    #return render(request, 'formulario_modelo_bicicleta.html')
     return render(request, 'formulario_modelo_bicicleta.html', { 'formulario': formulario })
 
 def formulario_cliente(request):
     if request.method == 'POST':
         formulario = ClienteFormulario(request.POST)
-        print(formulario)
-
+        
         if formulario.is_valid():
             datos = formulario.cleaned_data
-            cliente = Cliente(datos['nombre'], datos['apellido'], datos['email'], datos['edad'])
+            #cliente = Cliente(datos['nombre'], datos['apellido'], datos['email'], datos['edad'])
+            cliente = Cliente(nombre = datos['nombre'], apellido = datos['apellido'], email = datos['email'], edad = datos['edad'])
             cliente.save()
-            return render(request, 'inicio.html')    
+            return render(request, 'index.html')    
     
     formulario = ClienteFormulario(request.POST)
     return render(request, 'formulario_cliente.html',  {'formulario': formulario })
@@ -45,13 +46,17 @@ def formulario_cliente(request):
 def formulario_accesorio(request):
     if request.method == 'POST':
         formulario = AccesorioFormulario(request.POST)
-        print(formulario)
+     
 
         if formulario.is_valid():
             datos = formulario.cleaned_data
-            accesorio = Accesorio(datos['tipo'], datos['marca'], datos['descripcion'])
+            #print(datos) #Para debugging#
+            #accesorio = Accesorio(datos["tipo"], datos["marca"], datos["descripcion"])
+            accesorio = Accesorio(tipo = datos["tipo"], marca = datos["marca"], descripcion = datos["descripcion"])
+            
+            #print(accesorio) #Para debugging#
             accesorio.save()
-            return render(request, 'inicio.html')
+            return render(request, 'index.html')
 
     
     formulario = AccesorioFormulario(request.POST)
